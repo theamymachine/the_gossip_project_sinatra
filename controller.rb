6d61bce4e411b6b73bc7a0415769566f2d52a048
theamymachine <amymcarthur10@gmail.com>
2020-02-03 12:00:22 +0100

    first commit

---
+++ controller.rb
@@ -0,0 +1,15 @@
+require 'bundler'
+Bundler.require
+
+class ApplicationController < Sinatra::Base
+  
+  get '/' do 
+    erb :index
+  end 
+  get '/gossips/new/' do 
+    erb :new_gossip
+  end 
+
+end 
+
+
