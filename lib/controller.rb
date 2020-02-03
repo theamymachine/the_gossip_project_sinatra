7bf18af215c54404560fabf82535526ec1b132df
theamymachine <amymcarthur10@gmail.com>
2020-02-03 12:29:09 +0100

    up to 2.3

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,6 +1,17 @@
+require 'bundler'
+Bundler.require
+
 class ApplicationController < Sinatra::Base
+  
   get '/' do 
     erb :index
   end 
+  get '/gossips/new/' do 
+    erb :new_gossip
+  end 
+  post '/gossips/new/' do
+    puts "Ce programme ne fait rien pour le moment, on va donc afficher un message dans le terminal"
+  end
 end 
+
 
